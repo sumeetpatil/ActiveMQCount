2a6c97656f0f55049654d82527329d5fca3b9e75
sumeetpatil <sumeetp1991@gmail.com>
2016-03-22 23:10:32 +0530

    ActiveMQ Count

--- src/main/java/com/sumeet/qcount/Init.java
+++ src/main/java/com/sumeet/qcount/Init.java
@@ -1,14 +1,9 @@
 package com.sumeet.qcount;
 
 import java.io.IOException;
-
 import javax.jms.JMSException;
 import javax.naming.NamingException;
 
-/**
- * Hello world!
- *
- */
 public class Init 
 {
     public static void main( String[] args ) throws JMSException, NamingException, IOException
